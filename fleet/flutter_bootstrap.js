e3afdeb9646c544b29588b30dc711df65985b8e0
Abdullah KINIK <akinik456@gmail.com>
2026-09-14 22:17:09 +0300

    Update fleet web

--- fleet/flutter_bootstrap.js
+++ fleet/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1887487360" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1307306065" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
